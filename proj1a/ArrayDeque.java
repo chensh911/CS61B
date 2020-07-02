5c691a2a36601a6c40921819b4ee406a6f27bd4d
ChenShangheng <3500161@qq.com>
2020-07-02 11:10:25 +0800

    Finish Projext1A

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,95 @@
+import org.omg.CORBA.Object;
+
+import java.util.Objects;
+
+public class ArrayDeque <T> {
+    private T[] TArray;
+    private int size;
+
+    /**  Creates an empty array deque. */
+    public ArrayDeque(){
+        TArray = (T[])new Object[100];
+        size = 0;
+    }
+    /** resize an Arrary by multiply a factor*/
+    public void resize(int configure){
+        T[] NewTArray = (T[]) new Object[configure];
+        System.arraycopy(NewTArray,0,TArray,0,size);
+        TArray = NewTArray;
+    }
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(T item){
+        if(this.size()+1 > TArray.length) {
+            this.resize(size * 2);
+        }
+        int index = size;
+        while (index > 0){
+            TArray[index] = TArray[index - 1];
+            index --;
+        }
+        TArray[index] = item;
+        size ++;
+    }
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(T item){
+        if(this.size()+1 > TArray.length) {
+            this.resize(size * 2);
+        }
+        TArray[size] = item;
+        size ++;
+    }
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty(){
+        return (size == 0);
+    }
+    /** Returns the number of items in the deque. */
+    public int size(){
+        return size;
+    }
+    /** Prints the items in the deque from first to last,
+     *  separated by a space. */
+    public void printDeque(){
+        int index = 0;
+        while(index < size){
+            System.out.print(TArray[index] + " ");
+            size ++;
+        }
+    }
+    /** Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null. */
+    public T removeFirst(){
+        if(size == 0) return null;
+
+        int index = 0;
+        T temp = TArray[0];
+        while(index < this.size()){
+            TArray[index] = TArray[index +1];
+        }
+        TArray[size - 1] = null;
+        size--;
+        if(TArray.length / 4 > this.size()){
+            resize(size);
+        }
+        return temp;
+    }
+    /** Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null. */
+    public T removeLast(){
+        if(size == 0) return null;
+
+        T temp = TArray[size - 1];
+        TArray[size - 1] = null;
+        size--;
+        if(TArray.length / 4 > this.size()){
+            resize(size);
+        }
+        return temp;
+    }
+    /** Gets the item at the given index, where 0 is the front,
+     * 1 is the next item, and so forth. If no such item exists,
+     * returns null. Must not alter the deque! */
+    public T get(int index){
+        if(index >= size) return null;
+        else return TArray[index];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,113 @@
+public class LinkedListDeque <T> {
+    public class TNode {
+        private T item;
+        private TNode pre;
+        private TNode next;
+        public TNode(T x, TNode p, TNode n) {
+            item = x;
+            pre = p;
+            next = n;
+        }
+        public TNode() {
+            item = null;
+            pre = this;
+            next = this;
+        }
+    }
+    public TNode sentinel;
+    public int size;
+
+    /** Creates an empty linked list deque. */
+    public LinkedListDeque(){
+        sentinel = new TNode();
+        size = 0;
+    }
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(T item){
+        TNode temp = sentinel.next;
+        sentinel.next = new TNode(item, sentinel, temp);
+        size++;
+    }
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(T item){
+        TNode temp = sentinel.pre;
+        sentinel.pre = new TNode(item, temp, sentinel);
+        temp.next = sentinel.pre;
+        size ++;
+    }
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /** Returns the number of items in the deque. */
+    public int size(){
+        return size;
+    }
+    /** Prints the items in the deque from first to last, separated by a space. */
+    public void printDeque(){
+        if (this.size() == 0) return;
+
+        TNode last = sentinel.pre;
+        TNode ptr = sentinel.next;
+        while (ptr != last){
+            System.out.print(ptr.item + " ");
+            ptr = ptr.next;
+        }
+        System.out.println(last.item);
+    }
+    /** Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null. */
+    public T removeFirst(){
+        if (this.size() == 0) return null;
+
+        TNode ret = sentinel.next;
+        TNode temp = sentinel.next.next;
+        sentinel.next = temp;
+        temp.pre = sentinel;
+        size --;
+        return ret.item;
+    }
+    /** Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null. */
+    public T removeLast(){
+        if(this.size() == 0) return null;
+
+        TNode ret = sentinel.pre;
+        TNode temp = sentinel.pre.pre;
+        sentinel.pre = temp;
+        temp.next = sentinel;
+        size --;
+        return ret.item;
+    }
+    /** Gets the item at the given index, where 0 is the front,
+     * 1 is the next item, and so forth. If no such item exists,
+     * returns null.Must not alter the deque! */
+    public T get(int index){
+        if(this.size() < index - 1) return null;
+
+        int nowIndex = 0;
+        TNode ptr = sentinel.next;
+        while(nowIndex != index){
+            nowIndex ++;
+            ptr = ptr.next;
+        }
+        return ptr.item;
+    }
+    /** Same as get, but uses recursion. */
+    public T getRecursiveMock(int index, TNode node){
+        if (index == 0){
+            return node.item;
+        }else{
+            return getRecursiveMock(index-1, node.next);
+        }
+    }
+    public T getRecursive(int index){
+        if(this.size() < index - 1) return null;
+
+        return getRecursiveMock(index, sentinel.next);
+    }
+}
