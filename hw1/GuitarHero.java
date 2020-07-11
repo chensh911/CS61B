e89ebd784c86a749a612dfc8b2ff63b4cf7bfcb0
ChenShangheng <3500161@qq.com>
2020-07-11 11:05:15 +0800

    Finish HW1

---
+++ hw1/GuitarHero.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.StdAudio;
+import synthesizer.BoundedQueue;
+import synthesizer.GuitarString;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+public class GuitarHero {
+    private static String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        synthesizer.GuitarString[] sgs = new synthesizer.GuitarString[37];
+        for (int i = 0; i < keyboard.length(); i += 1) {
+            double frequency = 440 * Math.pow(2, (i - 24) * 0.5);
+            sgs[i] = new GuitarString(frequency);
+        }
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                if (keyboard.indexOf(key) != -1) {
+                    sgs[keyboard.indexOf(key)].pluck();
+                }
+            }
+            //compute superposition
+            double sample = 0;
+            Iterator<GuitarString> seer = new sgs.iterator();
+            for (GuitarString s: sgs) {
+                sample += s.sample();
+            }
+            //play
+            StdAudio.play(sample);
+            //tic
+            for (GuitarString s: sgs) {
+                s.tic();
+            }
+        }
+    }
+}
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,8 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+ package synthesizer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
-//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +19,9 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first = last = fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -30,6 +31,16 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (this.isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        if (last == capacity - 1) {
+            last = 0;
+        } else {
+            last += 1;
+        }
+        fillCount += 1;
     }
 
     /**
@@ -38,7 +49,19 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (this.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T temp = rb[first];
+        rb[first] = null;
+        if (first == capacity - 1) {
+            first = 0;
+        } else {
+            first += 1;
+        }
+        fillCount -= 1;
+        return temp;
     }
 
     /**
@@ -46,7 +69,34 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (this.isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        int index;
+        public ArrayRingBufferIterator() {
+            index = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            return index < fillCount;
+        }
+
+        @Override
+        public T next() {
+            T temp = rb[index];
+            index += 1;
+            return temp;
+        }
+    }
 }
